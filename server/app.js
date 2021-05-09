50655d616b71c9897c6da8dde63da8ec44b8ac95
jzhengcode <jzhengcode@gmail.com>
2021-05-08 19:54:17 -0700

    sppontacular find by ingredients connected

--- server/app.js
+++ server/app.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
 const express = require("express");
+const axios = require("axios");
 const path = require("path");
 
 const DIST_DIR = path.resolve(__dirname, "..", "client", "dist");
@@ -9,5 +11,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(DIST_DIR));
+
+function populateRecipeId(spoonacularData, recipeIds) {
+  for (let i = 0; i < spoonacularData.length; i += 1) {
+    const currentRecipe = spoonacularData[i]
+    recipeIds.push(currentRecipe.id)
+  }
+}
+
+app.get('/spoontacular', (req, res) => {
+  const recipeIds = [];
+  let { ingredients } = req.query;
+  
+  ingredients = ingredients.replace(/ /g, '+')
+  const findByIngredientsUrl = 'https://api.spoonacular.com/recipes/findByIngredients?';
+  const findByIngredientsParam = {
+    apiKey: process.env.SPOONTACULAR_APIKEY,
+    ingredients,
+    number: 2
+  }
+
+  axios.get(findByIngredientsUrl,{ params: findByIngredientsParam})
+    .then((response) => {
+      const { data } = response;
+      populateRecipeId(data, recipeIds);
+    })
+    .catch(error => {
+      res.send('spoontacular fail')
+    })
+})
 
 module.exports = app;
