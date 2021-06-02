f57e39bafdf90e024a397cc0a20d56436347172e
jzhengcode <jzhengcode@gmail.com>
2021-06-01 18:18:24 -0700

    recipes on populated with async thunk

---
+++ client/src/redux/recipesSlice.js
@@ -0,0 +1,30 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const getSpoonRecipesAsync = createAsyncThunk(
+  'recipes/getSpoonRecipesAsync',
+  async (list) => {
+    const ingredients = list.join(",+");
+    let recipes = {};
+    await axios
+      .get("spoontacular", { params: { ingredients } })
+      .then(({ data }) => {recipes = data})
+      .catch(err => console.log('getSpoonRecipes.js -- err:', err));
+
+    return {recipes}
+  }
+)
+
+export const recipesSlice = createSlice({
+  name: 'recipes',
+  initialState: {
+    recipes: {}
+  },
+  extraReducers: {
+    [getSpoonRecipesAsync.fulfilled]: (state,action) => {
+      return action.payload.recipes
+    }
+  }
+})
+
+export default recipesSlice.reducer;
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import ingredientsReducer from "./ingredientsSlice";
+import recipesReducer from './recipesSlice'
 
 export default configureStore({
   reducer: {
     ingredients: ingredientsReducer,
+    recipes: recipesReducer
   },
 });
